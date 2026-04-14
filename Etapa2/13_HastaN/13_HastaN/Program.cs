ba7d5384468ab87d6cfc26b3f9008ccb8bf89426
Yerik Limachi <limachiyerikk11@gmail.com>
2026-04-14 16:34:54 -0300

    Subo Etapa2 de tp 8 a 13

---
+++ Etapa2/13_HastaN/13_HastaN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número como limite: ");
+            int num = int.Parse(Console.ReadLine());
+            int contador = 1;
+            while (contador <= num)
+            {
+                if (contador % 2 == 0)
+                {
+                    Console.WriteLine(contador);
+                }
+                contador = contador + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa2/8_AñoBisiesto/8_AñoBisiesto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_AñoBisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca un Año: ");
+            int año = int.Parse(Console.ReadLine());
+            bool bi = false;
+            if (año % 4 == 0)
+            {
+                bi = true;
+                if (año % 100 == 0)
+                {
+                    bi = false;
+                    if (año % 400 == 0)
+                    {
+                        bi = true;
+                    }
+                }
+            }
+            if (bi == true)
+            {
+                Console.WriteLine("El año es bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine("El año no es bisiesto.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
